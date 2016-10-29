6ea3e6f08676b1387c1a048ae39d5c82931ec664
cel <celine.choo98@gmail.com>
2016-10-29 18:15:14 -0400

    started making grid view

---
+++ src/main/view.js
@@ -0,0 +1,42 @@
+/* 
+pixi - for grid
+*/
+
+var grid = PIXI.autoDetectRenderer(520, 520);
+document.body.appendChild(grid.view);
+var stage = new PIXI.Container();
+
+grid.view.style.border = "1px dashed black";
+grid.view.style.position = "absolute";
+grid.view.style.display = "block";
+
+var circle = new PIXI.Graphics();
+circle.beginFill(0x9966FF);
+circle.drawCircle(0, 0, 25);
+circle.endFill();
+circle.x = 120;
+circle.y = 120;
+stage.addChild(circle);
+
+
+/* setting up grid lines */
+var lines_y = [];
+var lines_x = [];
+
+for(var i=0; i<25; i=i+1){
+	lines_y[i] = new PIXI.Graphics();
+	lines_y[i].lineStyle(1, 0xFFFFFF, 1);
+	lines_y[i].moveTo(0,i*20);
+	lines_y[i].lineTo(520,i*20);
+	lines_y[i].x = 0; lines_y[i].y = i*20;
+	stage.addChild(lines_y[i]);
+
+	lines_x[i] = new PIXI.Graphics();
+	lines_x[i].lineStyle(1, 0xFFFFFF, 1);
+	lines_x[i].moveTo(i*20,0);
+	lines_x[i].lineTo(i*20,520);
+	lines_x[i].x = i*20; lines_x[i].y = 0;
+	stage.addChild(lines_x[i]);
+}
+
+grid.render(stage);
